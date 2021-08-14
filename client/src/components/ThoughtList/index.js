cf00ded2febdbbac5fababe5c4173e1e997c51fb
Shelbyrp <77222563+Shelbyrp@user.noreply.github.com>
2021-08-15 07:52:52 +0800

    trying to delete thought.

--- client/src/components/ThoughtList/index.js
+++ client/src/components/ThoughtList/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { useQuery } from '@apollo/client';
+import { QUERY_THOUGHTS } from '../../utils/queries';
+import { useMutation } from '@apollo/client';
+import { useParams } from 'react-router-dom';
+
 
 const ThoughtList = ({
   thoughts,
@@ -9,7 +14,23 @@ const ThoughtList = ({
 }) => {
   if (!thoughts.length) {
     return <h3>No Thoughts Yet</h3>;
+
+
   }
+
+  const { thoughtId } = useParams();
+
+  const { data } = useQuery(QUERY_THOUGHTS, {
+    // pass URL parameter
+    variables: { thoughtId: thoughtId },
+});
+console.log("data", data)
+
+  const [removeThought, { error }] = useMutation(REMOVE_THOUGHT, {
+    refetchQueries: [{ query: QUERY_THOUGHTS, variables: thoughtId }],
+    awaitRefetchQueries: true,
+});
+
 
   return (
     <div>
@@ -44,6 +65,13 @@ const ThoughtList = ({
               to={`/thoughts/${thought._id}`}
             >
               Join the discussion on this thought.
+            </Link>
+            <Link
+              className="btn btn-primary btn-block btn-squared"
+              to={`/thoughts/${thought._id}`}
+              onClick={() => removeThought({ variables: { id: thought._id } })}
+            >
+              Delete this thought
             </Link>
           </div>
         ))}
